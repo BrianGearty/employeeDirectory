322102a339a6a030efafa537df198ab574d8d12f
Brian GEARTY <geartybrian@gmail.com>
2020-11-10 13:06:23 -0500

    updated handleTyping and handleAlphabet in App.js

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,32 +1,39 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
 import Axios from 'axios';
-import Table from './components/Table/Table'
-
-
+// import Table from './components/Table'
+// import TableHeader from "./components/TableHeader";
 
 
 function App() {
-
+// HOOK
   const [state, setState] = useState({
     employees: [],
-    filteredEmps: []
+    filteredEmps: [],
+    alphabetizedEmps: []
   })
-
+// GETTING API RESULTS BEFORE PAGE LOADS
   useEffect(() => {
     Axios.get('https://randomuser.me/api/?results=200&nat=us').then(function (data) {
-      console.log('data from api', data.data.results)
       setState({ ...state, employees: data.data.results })
     })
   }, [])
 
+  // ALPHABETIZATION OF EMPLOYEES
   const handleAlphabet = () => {
+    var newAlphabetizedName = []
 
-
+    for (var i = 0; i < state.employees.length; i++) {
+    newAlphabetizedName.push(state.employees[i]);
+  }
+    
+  // console.log('state emps', newAlphabetizedName.sort() )
+    setState({...state, alphabetizedEmps: newAlphabetizedName})
   }
 
+// HANDLE TYPING TO SORT EMPLOYEES  
   const handleTyping = (event) => {
-    console.log("were typing", event.target.value)
+    console.log("we're typing", event.target.value)
     var newFiltered = []
 
     for (var i = 0; i < state.employees.length; i++) {
@@ -39,36 +46,57 @@ function App() {
     setState({ ...state, filteredEmps: newFiltered })
 
   }
-
+// IF STATEMENT FOR TYPING TO SORT EMPLOYEES
   var empsToDisplay = state.employees
 
   if (state.filteredEmps.length > 0) {
     empsToDisplay = state.filteredEmps
   }
 
-  console.log('this is our state!!', state)
+// STATEMENT FOR ALPHABETIZING NAMES
+  for(var i =0; i < state.alphabetizedEmps.length; i ++){
+    if(state.alphabetizedEmps.length > 0){
+      empsToDisplay = state.alphabetizedEmps
+
+      empsToDisplay.sort(function(a, b){
+        if (a.name.first < b.name.first) { return -1;}
+        if (a.name.first > b.name.first) { return 1; }
+        return 0;
+      })
+    }
+  }
+
   return (
     <div className="App">
-      <input onChange={handleTyping}></input>
+      <h4>Employee Name</h4><input style={{ width: "200px"}} onChange={handleTyping}></input>
 
-      <table style={{ width: "100%" }}>
+      <table style={{ width: "100%", border: "2px solid black"}}>
+        <tbody>
         <tr>
           <th><h2>Photo</h2></th>
-          <th><h2 onChange={handleAlphabet}>Name</h2></th>
+          <th><h2 onClick={handleAlphabet}>Name</h2></th>
           <th><h2>Email</h2></th>
           <th><h2>Phone</h2></th>
           <th><h2>DOB</h2></th>
         </tr>
-        
-          {empsToDisplay.map((emp) => {
-            return (
-              <>
-              <Table></Table>
-              </>
-            )
-          })
+        </tbody>
+        {empsToDisplay.map((emp) => {
+          return (
+            <>
+            <tbody>
+              <tr>
+                <td style={{border: "2px solid black"}}><img alt="Employee" src={emp.picture.thumbnail}></img></td>
+                <td><h3>{emp.name.first} {emp.name.last}</h3></td>
+                <td><h4>{emp.email}</h4></td>
+                <td><h4>{emp.phone}</h4></td>
+                <td><h4>{emp.dob.date.slice(0, 10)}</h4></td>
+              </tr>
+              </tbody>
+            </>
+          )
+        })
         }
-        
+
       </table>
     </div>
   )
---
+++ employeedirectory/src/components/TableHeader/index.js
@@ -0,0 +1,15 @@
+// import React from "react";
+
+// function TableHeader(props) {
+//     return(
+//     <tr>
+//         <th><h2>Photo</h2></th>
+//         <th><h2 onChange={props.handleAlphabet}>Name</h2></th>
+//         <th><h2>Email</h2></th>
+//         <th><h2>Phone</h2></th>
+//         <th><h2>DOB</h2></th>
+//     </tr>
+//     )
+// }
+
+// export default TableHeader;
